3c7bbd3eeee4658c47cfd5ffb648f31b102e25bf
rahullym <rahul@lymdata.com>
2026-04-08 08:52:35 +0530

    Fix: Explicitly define NEXTAUTH_SECRET in authOptions for production

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,27 +1,10 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
-
-const connectionString = process.env.DATABASE_URL
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined in environment.")
-}
-
-const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
-
-const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter })
-}
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
+  prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
